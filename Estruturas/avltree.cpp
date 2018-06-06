0bdb7bed5f710d6309ffb2d8a1d183a1ae97c89f
Karla Félix <karlakaroline_7@hotmail.com>
2018-06-06 07:09:45 -0400

    123456

--- Estruturas/avltree.cpp
+++ Estruturas/avltree.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 template <typename I>
-class Item(){
+class Item{
 private:
   I item;
   int chave;
@@ -39,6 +39,7 @@ private:
   Item item;
 
   int altura;
+  int balance;
 
 public:
   No(No<N>* pai){
@@ -89,6 +90,14 @@ public:
     return altura;
   }
 
+  void setBalance(int balance){
+    this->balance = balance;
+  }
+
+  int getBalance(){
+    return balance;
+  }
+
 
 };
 
@@ -96,12 +105,13 @@ template <typename T>
 class AVLtree{
 private:
 
-  No<T>* raiz;
+  No<T>* raiz, navegador;
 
 public:
 
   AVLtree(){
     this->raiz->setPai(NULLPTR);
+    navegador = raiz;
   }
 
   void busca(T item){
@@ -113,15 +123,101 @@ public:
     else if(aux->getItemNo().getChave() > )
   }
 
-  void insercao(T item){
-    No<T>* aux = new No<T>();
-    aux = raiz;
+  void insercaoAVL(T item, int pos, No<T>* aux){
+
+    int cresceu; //VARIAVEL PARA SABER SE PRECISA BALANCEAR
+    //USA NO LUGAR DO BOOLEANO
+
+    //SE A ARVORE VAZIA
     if (aux == NULL){
-      raiz.setItem(item);
-      raiz.setAltura(0);
+      aux->setChave(pos);
+      aux->setDir(NULL);
+      aux->setEsq(NULL);
+      aux->setBalance(0);
+
+      cresceu = 1;
     }
+    else if(aux->getChave() > pos){
+      //TENTA INSERIR NA ARVORE ESQUERDA E VE SE CRESCEU
+      cresceu = insereAVL(item, pos, &aux->getEsq());
+      if(cresceu){
+        switch(aux->getBalance()){
 
+        //CASO A ARV DIR SEJA MAIOR, N HA CRESCIMENTO
+        case 1:
+          aux->setBalance(0);
+          cresceu = 0;
+          break;
+        //CASO A ARV DIREITA TAMANHO IGUAL HOUVE CRESCIMENTO
+        case 0:
+          aux->setBalance(-1);
+          cresceu = 1;
+          break;
+        //CASO A ESQ SEJA MAIOR REBALANCEAR
+        case -1:
+          if(aux->getEsq()->getBalance() == -1){
+            rotacaoDireita(aux);
+            aux->setBalance(0);
+            aux->getDir()->setBalance(0);
+          }
+          else{
+            //ROTACAO DUPLA
+            rotacaoEsquerda(&aux->getEsq());
 
+            rotacaoDireita(aux);
+            if(aux->getBalance() == -1){
+              aux->getEsq()->setBalance(0);
+              aux->getDir()->setBalance(1);
+            }
+            else{
+              aux->getDir()->setBalance(0);
+              aux->getEsq()->setBalance(-aux->getBalance());
+            }
+            aux->setBalance(0);
+          }
+          cresceu = 0;
+        }
+      }
+    }
+    else if(aux->getChave() < x){
+      cresceu = insereAVL(item, pos, &aux->getDir());
+      if (cresceu){
+        switch(aux->getBalance()){
+        case -1:
+          aux->setBalance(0);
+          cresceu = 0;
+          break;
+        case 0:
+          aux->setBalance(1);
+          cresceu = 1;
+          break;
+        case 1:
+          if(aux->getDir()->getBalance() == 1){
+            rotacaoEsquerda(aux);
+
+            aux->setBalance(0);
+            aux->getEsq()->setBalance(0);
+          }
+          else{
+            rotacaoDireita(&aux->getDir());
+            rotacaoEsquerda(aux);
+            if(aux->getBalance() == -1){
+              aux->getEsq()->setBalance(0);
+              aux->getDir()->setBalance(1);
+            }
+            else{
+              aux->getDir()->setBalance(0);
+              aux->getEsq()->setBalance(- aux->getBalance());
+            }
+            aux->setBalance(0);
+          }
+          cresceu = 0;
+        }
+      }
+    }
+    else cresceu = 0;
+
+    return cresceu;
   }
 
 
